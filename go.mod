ef5fdf2be46c2c6438f8239fc0d23ec4ceb20921
NAGARJUNA <nagarjunanani333@gmail.com>
2025-01-23 11:54:46 +0530

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nagarjuna2323/news
+
+go 1.23.5
---
+++ hotTopic/BreakingNews.go
@@ -0,0 +1,7 @@
+package hottopic
+
+import "fmt"
+
+func BreakingNews() {
+	fmt.Println("Airports Authority of India expects the construction work to be completed by March 2026 . An official cites challenging excavation, owing to hard rock at the site, cost escalation and issues with project contractor for delay in completion")
+}
