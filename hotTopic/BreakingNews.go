84315b74058a9e7a2a056be82baa03ea2c972801
NAGARJUNA <nagarjunanani333@gmail.com>
2025-01-24 11:34:47 +0530

    Second Commit

--- hotTopic/BreakingNews.go
+++ hotTopic/BreakingNews.go
@@ -1,7 +1,12 @@
 package hotTopic
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
 
 func BreakingNews() {
+	color.Red("Airports Authority of India expects the construction work to be completed by March 2026 ")
 	fmt.Println("Airports Authority of India expects the construction work to be completed by March 2026 . An official cites challenging excavation, owing to hard rock at the site, cost escalation and issues with project contractor for delay in completion")
 }
